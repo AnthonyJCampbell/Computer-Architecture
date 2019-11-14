b0ccff03c584641e6bab25b0ffcfdd79d272a11f
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-11-14 08:47:07 +0100

    Implementing branch table

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -6,40 +6,61 @@ class CPU:
     """Main CPU class."""
 
     def __init__(self):
-        """Construct a new CPU."""
         self.reg = [0] * 8
         self.ram = [0] * 256
         self.pc = 0
 
         self.branchtable = {}
-        self.branchtable[0b01000111] = self.prn # 71 - Print
+        self.branch_operations()
 
     def load(self, program):
-        """Load a program into memory."""
-
         address = 0
-
-        # For now, we've just hardcoded a program:
 
         for instruction in program:
             self.ram[address] = instruction
             address += 1
-
         # print(f"self.ram is loaded with instructions, it currently looks like: {self.ram}")
+
+
+    #############
+    # BRANCH OP #
+    #############
+    # * `LDI`: load "immediate", store a value in a register, or "set this register to this value".
+    def LDI(self, a, b):
+        self.reg[a] = b
+        self.pc += 3
+
+    # "PRN". `PRN`: a pseudo-instruction that prints the numeric value stored in a register.
+    def PRN(self, a, b):
+        print(self.reg[a])
+        self.pc += 2
+
+    def MUL(self, a, b):
+        self.alu("MUL", a, b)
+        self.pc += 3
+
+    def branch_operations(self):
+        self.branchtable[0b10000010] = self.LDI
+        self.branchtable[0b01000111] = self.PRN
+        self.branchtable[0b10100010] = self.MUL
+
+
+
 
     # Returns the value found at the address in memory
     def ram_read(self, address):
         return self.ram[address]
 
+
+
     def ram_write(self, address, data):
         self.ram[address] = data
 
-    def alu(self, op, reg_a, reg_b):
-        """ALU operations."""
 
+
+    def alu(self, op, reg_a, reg_b):
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
 
         elif op == "MUL":
             self.reg[reg_a] *= self.reg[reg_b]
@@ -71,16 +92,6 @@ class CPU:
 
 
 
-    # sets a specified register to a specified value
-    def ldi(self, reg_a, data):
-        self.reg[reg_a] = data
-
-
-
-    # Print the value at the designated register address
-    def prn(self, reg):
-        print(self.reg[reg])
-
     def run(self):
         """Run the CPU."""
         # We're extracting the instructions from RAM it seems
@@ -99,24 +110,11 @@ class CPU:
                 print("Closing run loop")
                 active = False
                 break
-            
-            # "PRN". `PRN`: a pseudo-instruction that prints the numeric value stored in a register.
-            elif IR == 0b01000111:
-                self.branchtable[IR](operand_a)
-                self.pc += 2
 
-            elif IR == 0b10100010:
-                self.alu("MUL", operand_a, operand_b)
-                self.pc += 3
-
-            # * `LDI`: load "immediate", store a value in a register, or "set this register to this value".
-            elif IR == 0b10000010:
-                # print(f"Storing {operand_b} in register[{operand_a}]")
-                self.ldi(operand_a, operand_b)
-                self.pc += 3
-
-            else:
+            elif IR not in self.branchtable:
                 print(f"Invalid instruction {IR}")
                 sys.exit(1)
 
+            else:
+                self.branchtable[IR](operand_a, operand_b)
 
